faf4a7d0025fcf8d08aeb77ddc0311259caf4db9
cemd <99793829+cemdusenkalkan@users.noreply.github.com>
2023-06-18 17:11:23 +0300

    Update prototype.py
    forgot to write a take profit order lol
    
    just trying to backtest with random values with ATR

--- prototype.py
+++ prototype.py
@@ -1,5 +1,6 @@
 import talib
 import numpy as np
+import pandas as pd
 
 
 def calculate_GMMA(close_prices, short_periods, long_periods):
@@ -13,51 +14,96 @@ def calculate_ATR(high_prices, low_prices, close_prices, atr_period):
     return atr
 
 
-# Custom price data for backtesting
-close_prices = np.array([100, 102, 105, 101, 98, 105, 110, 108, 112, 115, 110, 105])
-high_prices = np.array([105, 108, 110, 105, 102, 110, 115, 112, 116, 118, 112, 108])
-low_prices = np.array([95, 98, 100, 96, 94, 100, 105, 102, 106, 110, 105, 100])
+def backtest_strategy(csv_file, short_periods, long_periods, atr_period):
+    df = pd.read_csv(csv_file)
+    close_prices = df['Close'].values
+    high_prices = df['High'].values
+    low_prices = df['Low'].values
 
+    short_emas, long_emas = calculate_GMMA(close_prices, short_periods, long_periods)
+
+    atr = calculate_ATR(high_prices, low_prices, close_prices, atr_period)
+
+    position = None
+    stop_loss = None
+    take_profit = None
+
+    total_trades = 0
+    winning_trades = 0
+    losing_trades = 0
+    pnl = []
+
+    for i in range(len(close_prices)):
+        if i < max(long_periods):
+            continue
+
+        # long position
+        if all(short_emas[j][i] > long_emas[j][i] for j in range(len(short_emas))):
+            if position is None or position < 0:
+                print(f"Open long position at price {close_prices[i]}")
+                position = 1
+                stop_loss = close_prices[i] - 2 * atr[i]
+                take_profit = close_prices[i] + 4 * atr[i]
+
+        # short position
+        elif all(short_emas[j][i] < long_emas[j][i] for j in range(len(short_emas))):
+            if position is None or position > 0:
+                print(f"Open short position at price {close_prices[i]}")
+                position = -1
+                stop_loss = close_prices[i] + 2 * atr[i]
+                take_profit = close_prices[i] - 4 * atr[i]
+
+        # update stop loss
+        if position == 1:  # long position
+            stop_loss = max(stop_loss, close_prices[i] - 2 * atr[i])
+        elif position == -1:  # short position
+            stop_loss = min(stop_loss, close_prices[i] + 2 * atr[i])
+
+        # check for stop loss hit
+        if position == 1 and close_prices[i] <= stop_loss:
+            print(f"Stop loss hit for long position at price {close_prices[i]}")
+            position = None
+            stop_loss = None
+            take_profit = None
+            losing_trades += 1
+            pnl.append(-2 * atr[i])
+        elif position == -1 and close_prices[i] >= stop_loss:
+            print(f"Stop loss hit for short position at price {close_prices[i]}")
+            position = None
+            stop_loss = None
+            take_profit = None
+            losing_trades += 1
+            pnl.append(-2 * atr[i])
+
+        # check for take profit hit
+        if position == 1 and close_prices[i] >= take_profit:
+            print(f"Take profit hit for long position at price {close_prices[i]}")
+            position = None
+            stop_loss = None
+            take_profit = None
+            winning_trades += 1
+            pnl.append(4 * atr[i])
+        elif position == -1 and close_prices[i] <= take_profit:
+            print(f"Take profit hit for short position at price {close_prices[i]}")
+            position = None
+            stop_loss = None
+            take_profit = None
+            winning_trades += 1
+            pnl.append(4 * atr[i])
+
+    total_trades = winning_trades + losing_trades
+    average_pnl = np.mean(pnl) if pnl else 0
+
+    print(f"Total Trades: {total_trades}")
+    print(f"Winning Trades: {winning_trades}")
+    print(f"Losing Trades: {losing_trades}")
+    print(f"Average P/L per trade: {average_pnl}")
+
+
+
+csv_file = '/Users/cem/Desktop/binance1h.csv'
 short_periods = [2, 7, 11, 13, 21, 14]
 long_periods = [29, 31, 37, 47, 53, 61]
+atr_period = 14
 
-short_emas, long_emas = calculate_GMMA(close_prices, short_periods, long_periods)
-
-atr = calculate_ATR(high_prices, low_prices, close_prices, atr_period=14)
-
-position = None
-stop_loss = None
-
-for i in range(len(close_prices)):
-    if i < max(long_periods):
-        continue
-
-    # long position
-    if short_emas[0][i] > long_emas[0][i] and short_emas[1][i] > long_emas[1][i] and short_emas[2][i] > long_emas[2][i]:
-        if position is None or position < 0:
-            print(f"Open long position at price {close_prices[i]}")
-            position = 1
-            stop_loss = close_prices[i] - 2 * atr[i]
-
-    # short position
-    elif short_emas[0][i] < long_emas[0][i] and short_emas[1][i] < long_emas[1][i] and short_emas[2][i] < long_emas[2][i]:
-        if position is None or position > 0:
-            print(f"Open short position at price {close_prices[i]}")
-            position = -1
-            stop_loss = close_prices[i] + 2 * atr[i]
-
-    # update stoploss
-    if position == 1:  # long position
-        stop_loss = max(stop_loss, close_prices[i] - 2 * atr[i])
-    elif position == -1:  # short position
-        stop_loss = min(stop_loss, close_prices[i] + 2 * atr[i])
-
-    # to check for sl
-    if position == 1 and close_prices[i] <= stop_loss:
-        print(f"Stop loss hit for long position at price {close_prices[i]}")
-        position = None
-        stop_loss = None
-    elif position == -1 and close_prices[i] >= stop_loss:
-        print(f"Stop loss hit for short position at price {close_prices[i]}")
-        position = None
-        stop_loss = None
+backtest_strategy(csv_file, short_periods, long_periods, atr_period)
